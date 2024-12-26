e532f1482ca7d5da145a6f9b8b5a060452702f58
Sanket Bhuite <sanketbhuite75245@gmail.com>
2024-12-26 15:14:55 +0530

    Update currency.py

--- currency.py
+++ currency.py
@@ -1,25 +1,28 @@
 currency = {
     "INR": 1,
-    "USD": 0.01197,
-    "EUR": 0.01119,
-    "GBP": 0.00953,
-    "JPY": 1.91,
-    "AUD": 0.01753,
-    "CAD": 0.01633,
-    "CNY": 0.08673,
-    "CHF": 0.01084,
-    "SGD": 0.01622,
-    "NZD": 0.01877,
+    "USD": 0.012,
+    "EUR": 0.011,
+    "GBP": 0.0095,
+    "JPY": 1.90,
+    "AUD": 0.0175,
+    "CAD": 0.0163,
+    "CNY": 0.0867,
+    "CHF": 0.0108,
+    "SGD": 0.0162,
+    "NZD": 0.0188,
     "ZAR": 0.214,
     "RUB": 1.14,
-    "AED": 0.04400,
-    "SAR": 0.04396,
-    "HKD": 0.09581,
-    "MYR": 0.05437,
-    "THB": 0.422,
+    "AED": 0.044,
+    "SAR": 0.044,
+    "HKD": 0.0958,
+    "MYR": 0.0544,
+    "THB": 0.0422,
     "KRW": 1.57,
     "IDR": 191.47,
     "MXN": 0.213
 }
-names=["INR", "USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CNY", "CHF", "SGD", "NZD", "ZAR", "RUB", "AED", "SAR", "HKD", "MYR", "THB", "KRW", "IDR", "MXN"
+
+names = [
+    "INR", "USD", "EUR", "GBP", "JPY", "AUD", "CAD", "CNY", "CHF", "SGD",
+    "NZD", "ZAR", "RUB", "AED", "SAR", "HKD", "MYR", "THB", "KRW", "IDR", "MXN"
 ]
